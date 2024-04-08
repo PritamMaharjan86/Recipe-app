6ee1f7ec30c32fd8281b7d1f6687c66094f08865
Pritam Maharjan <primgdev@gmail.com>
2024-04-08 21:03:15 +1000

    package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [recipe, setRecipe] = useState("");
+
+  const handleChange = (e) => {
+    setRecipe(e.target.value);
+    console.log("recipe", recipe);
+  };
+
+  const detectKey = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = async () => {
+    const api_url = `https://api.api-ninjas.com/v1/recipe?query=${recipe}`;
+    try {
+      const response = await fetch(api_url, {
+        method: "get",
+        headers: {
+          "X-Api-Key": "6rZ6rEmt1bGPz9YUP31rPA==QK5ias8tqlCLrFxZ",
+        },
+      });
+      var data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-bar">
+        <input
+          className="input"
+          type="text"
+          placeholder="Search for recipe"
+          onChange={(e) => handleChange(e)}
+          onKeyUp={(e) => detectKey(e)}
+        ></input>
+      </div>
     </div>
   );
 }
