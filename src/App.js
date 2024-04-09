403fb18eff4f1219284775c066a3028e4a368b63
Pritam Maharjan <primgdev@gmail.com>
2024-04-09 19:20:28 +1000

    feature/UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [recipe, setRecipe] = useState("");
+  const [output, setOutput] = useState([]);
 
   const handleChange = (e) => {
     setRecipe(e.target.value);
@@ -17,6 +18,7 @@ function App() {
   };
 
   const handleSubmit = async () => {
+    // calling API with headers
     const api_url = `https://api.api-ninjas.com/v1/recipe?query=${recipe}`;
     try {
       const response = await fetch(api_url, {
@@ -25,13 +27,15 @@ function App() {
           "X-Api-Key": "6rZ6rEmt1bGPz9YUP31rPA==QK5ias8tqlCLrFxZ",
         },
       });
-      var data = await response.json();
-      console.log(data);
+
+      const data = await response.json();
+
+      setOutput(data);
     } catch (error) {
       console.log(error);
     }
   };
-
+  console.log("output", output);
   return (
     <div className="App">
       <div className="search-bar">
@@ -39,9 +43,31 @@ function App() {
           className="input"
           type="text"
           placeholder="Search for recipe"
+          // to get event from handleChange
           onChange={(e) => handleChange(e)}
           onKeyUp={(e) => detectKey(e)}
         ></input>
+      </div>
+
+      <div className="display">
+        <h2>Recipes</h2>
+
+        {console.log(output)}
+        {output.map((data, index) => {
+          return (
+            <>
+              <div className="board" key={index}>
+                <span>Title: {data.title}</span>
+                <br />
+                <span>Ingredients: {data.ingredients}</span>
+                <br />
+                <p>Instructions: {data.instructions}</p>
+                <br />
+              </div>
+              <br />
+            </>
+          );
+        })}
       </div>
     </div>
   );
