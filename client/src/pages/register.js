2cc19f6dda0d2b6be54419d4c6702b52557958bd
Pritam Maharjan <primgdev@gmail.com>
2024-04-28 10:38:34 +1000

    feature/14/registration_form/backend

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,24 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./register.css";
+import http from "../pages/http";
+import { resolvePath, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Register() {
-  return (
-    <div className="container">
-    <div className="register">
-      <div className="column1">
-        <img
-          className="logo"
-          src="https://res.cloudinary.com/dvmumi2mb/image/upload/v1713698554/recipe_pcnsla.png"
-        ></img>
-        
-        <h1>Register Here</h1>
-        <div><input className='email_register' placeholder='Email' type='text'></input></div>
-        <div><input className='password_register' placeholder='Create password' type='password'></input></div>
-        <div><button type='submit'>Register</button></div>
-        
-      </div>
 
-      </div>
-      </div>
-  )
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+        console.log('username:', username);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        console.log('password:', password);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await http.post('/auth/create-user', { username, password });
+            console.log(response);
+            setIsLoggedIn(true);
+
+        } catch (error) {
+            toast.error(error.response.data.error.message);
+            console.error(error.response);
+
+        }
+    };
+
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            console.log('isloggedin', isLoggedIn)
+            navigate('/home');
+        }
+    }, [isLoggedIn])
+
+    return (
+        <>
+            <ToastContainer />
+            <div className="container">
+                <div className="register">
+                    <div className="column1">
+                        <img
+                            className="logo"
+                            src="https://res.cloudinary.com/dvmumi2mb/image/upload/v1713698554/recipe_pcnsla.png"
+                        ></img>
+
+                        <h1>Register Here</h1>
+                        <div><input className='name_register' placeholder='Username' type='text' onChange={handleUsername}></input></div>
+                        <div><input className='password_register' placeholder='Create password' type='password' onChange={handlePassword}></input></div>
+                        <div><button type='submit' onClick={handleSubmit}>Register</button></div>
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+
+    )
 }
